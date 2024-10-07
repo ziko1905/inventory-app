cac465e81c958e8cc57bc4b1d64b27714f1993db
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-07 23:33:34 +0200

    WIP - render single product info

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,18 @@
+const db = require("../db/queries")
+
 function productsListGet(req, res) {
     // empty array until db is created
     res.render("listProducts", {title: "Products list", categories: [], products: []})
 }
 
+async function productGet(req, res) {
+    const id = req.params.id
+    const {product, categories} = db.getProduct(id)
+    if (!product) throw `Couldn't find product with id: ${id}`
+    res.render('product', { title: `${product.name} - info`, info: product, infoArr: categories})
+}
+
 module.exports = {
     productsListGet,
+    productGet,
 }
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -3,5 +3,6 @@ const productsRouter = Router()
 const productsController = require("../controllers/productsController")
 
 productsRouter.get("/", productsController.productsListGet)
+productsRouter.get("/:id", productsController.productGet)
 
 module.exports = productsRouter
