50f8c6a836fee0ad57ea58455e62a8e6b239e36a
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-07 23:21:46 +0200

    Create query for getting the product info

---
+++ db/queries.js
@@ -0,0 +1,22 @@
+const pool = require("./pool")
+
+async function getProduct(id) {
+    const productPromise = new Promise((resolve, reject) => {
+        pool.query("SELECT * FROM products WHERE id = $1", id)
+        .then(value => resolve(value.rows))
+        .catch(err => reject(err))
+    })
+
+    const categoriesPromise = new Promise((resolve, reject) => {
+        pool.query("SELECT name FROM categories JOIN linker ON categories.id = linker.category_id JOIN products ON products.id = linker.product_id")
+        .then(value => resolve(value.rows))
+        .catch(err => reject(err))
+    })
+    
+    const { product, categoriesList } = await Promise.all([productPromise, categoriesPromise])
+    return [product, categoriesList]
+}
+
+module.exports = {
+    getProduct,
+}
