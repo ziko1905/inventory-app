4f385e988ad2837360535618ad5a8158fd44f891
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-08 21:42:43 +0200

    Make populate insert dummy values

--- db/populate.js
+++ db/populate.js
@@ -1,5 +1,5 @@
 #! usr/bin/env node
-const { Client } = require("pg")
+const { Client } = require('pg')
 
 const CREATE_PRODUCTS_TABLE = `CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -15,10 +15,25 @@ const CREATE_CATEGORIES_TABLE = `CREATE TABLE IF NOT EXISTS categories (
     name VARCHAR ( 20 )
 );`
 
-const SQL = CREATE_PRODUCTS_TABLE + CREATE_CATEGORIES_TABLE
+const INSERT_CATEGORIES = `INSERT INTO categories (name)
+    VALUES
+    ('Bikes'),
+    ('Clothing')
+;`
+
+const INSERT_PRODUCTS = `INSERT INTO products 
+    (name, stock_amount, size, color, category_id) VALUES
+        ('Pressure Gauge', 10, NULL, 'Black', NULL),
+        ('Helmet', 4, 'L', 'Green', NULL),
+        ('Hat', 15, 'M', 'Blue', 2),
+        ('Specialized Enduro', 3, 'XL', 'Blue', 1),
+        ('Trousers', 7, 'L', 'Red', 2)
+;`
+
+const SQL = CREATE_PRODUCTS_TABLE + CREATE_CATEGORIES_TABLE + INSERT_CATEGORIES + INSERT_PRODUCTS
 
 const main = async (connectionString=process.argv[2]) => {
-    console.log("sending...")
+    console.log('sending...')
     const client = new Client({
         connectionString
     })
@@ -26,7 +41,7 @@ const main = async (connectionString=process.argv[2]) => {
     await client.connect()
     await client.query(SQL)
     await client.end()
-    console.log("done")
+    console.log('done')
 }
 
 main()
