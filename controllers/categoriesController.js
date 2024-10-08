3e2c39323146e5556d7348750fe8e39023076e27
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-08 17:43:38 +0200

    Create routing and controller for category info

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,7 +1,19 @@
+const db = require("../db/queries")
+const asyncHandler = require("express-async-handler")
+const { NotFoundError } = require("../errors")
+
 function categoriesListGet(req, res) {
     res.render("listCategories", {title: "Categories list", categories: []})
 }
 
+const categoryGet = asyncHandler(async (req, res) => {
+    const id = req.params.categoryId
+    const category = await db.getCategory(id)
+    if (!category) throw new NotFoundError("Couldn't find category with id:", id)
+    res.render("category", {info: category})
+})
+
 module.exports = {
     categoriesListGet,
+    categoryGet,
 }
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,5 +3,6 @@ const categoriesRouter = Router()
 const categoriesController = require("../controllers/categoriesController")
 
 categoriesRouter.get("/", categoriesController.categoriesListGet)
+categoriesRouter.get("/:categoryId", categoriesController.categoryGet)
 
 module.exports = categoriesRouter
