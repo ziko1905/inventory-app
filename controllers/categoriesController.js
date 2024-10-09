8731442ebb3a896d061cd22e8e265cd68228a609
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-09 17:54:21 +0200

    Make category update load category

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -18,8 +18,16 @@ function categoryCreateGet(req, res) {
     res.render("createCategory", {title: "Create Category"})
 }
 
+const categoryUpdateGet = asyncHandler(async (req, res) => {
+    const id = req.params.categoryId
+    const category = await db.getCategory(id)
+    if (!category) throw new NotFoundError(`Couldn't find category with id: ${id}`)
+    res.render("updateCategory", {title: `${category.name} - category update`, category: category})
+})
+
 module.exports = {
     categoriesListGet,
     categoryGet,
     categoryCreateGet,
+    categoryUpdateGet,
 }
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -5,5 +5,6 @@ const categoriesController = require("../controllers/categoriesController")
 categoriesRouter.get("/", categoriesController.categoriesListGet)
 categoriesRouter.get("/create", categoriesController.categoryCreateGet)
 categoriesRouter.get("/:categoryId", categoriesController.categoryGet)
+categoriesRouter.get("/:categoryId/update", categoriesController.categoryUpdateGet)
 
 module.exports = categoriesRouter
