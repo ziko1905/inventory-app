6cd888ac95be63a64b9b4fa317d8ea6ba475d27f
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-10 20:58:20 +0200

    Fix not being able to link to category

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -4,7 +4,7 @@ const categoriesController = require("../controllers/categoriesController")
 
 categoriesRouter.get("/", categoriesController.categoriesListGet)
 categoriesRouter.get("/create", categoriesController.categoryCreateGet)
-categoriesRouter.get("/:categoryId", categoriesController.categoryGet)
+categoriesRouter.get("/:categoryId/detail", categoriesController.categoryGet)
 categoriesRouter.get("/:categoryId/update", categoriesController.categoryUpdateGet)
 categoriesRouter.post("/:categoryId/update", categoriesController.categoryUpdatePost)
 categoriesRouter.post("/create", categoriesController.categoryCreatePost)
