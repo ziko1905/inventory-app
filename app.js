429f1edfb77fd6f2bc5b3bc0b69ec795fc78dd8f
Karlo Cehulic <karlocehlic@gmail.com>
2024-10-10 22:40:28 +0200

    Fix urlencoded

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
 app.use(express.static('public'))
-app.use(express.urlencoded({extends: true}))
+app.use(express.urlencoded({extended: true}))
 app.use("/", productsRouter)
 app.use("/categories", categoriesRouter)
 app.use((err, req, res, next) => {
